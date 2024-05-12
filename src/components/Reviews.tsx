02682fca5b213f383f6b6c9f0c3973f7fd96ad95
Kavishcan <kavishcan2002@gmail.com>
2024-05-13 01:10:14 +0530

    chore: Add framer-motion library for animations and update testimonials section

---
+++ src/components/Reviews.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useRef } from "react";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import { useInView } from "framer-motion";
+
+function ReviewGrid() {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  return (
+    <div
+      ref={containerRef}
+      className="relative -mx-4 mt-16 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3"
+    ></div>
+  );
+}
+
+export function Reviews() {
+  return (
+    <MaxWidthWrapper className="relative max-w-5xl">
+      <img
+        aria-hidden="true"
+        src="/what-people-are-buying.png"
+        className="absolute select-none hidden xl:block -left-32 top-1/3"
+      />
+
+      <ReviewGrid />
+    </MaxWidthWrapper>
+  );
+}
